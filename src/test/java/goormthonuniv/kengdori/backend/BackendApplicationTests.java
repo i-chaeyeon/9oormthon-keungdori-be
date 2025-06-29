0f3133df32181a2e5dd4dc27a82b9d143be0ba85
i-chaeyeon <leechae0605@naver.com>
2025-06-29 16:57:04 +0900

    initial commit

---
+++ src/test/java/goormthonuniv/kengdori/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package goormthonuniv.kengdori.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
