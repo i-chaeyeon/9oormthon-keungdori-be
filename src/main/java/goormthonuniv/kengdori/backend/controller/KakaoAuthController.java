05a7a64824102afaada7b1b8c35b979031c23e4a
i-chaeyeon <leechae0605@naver.com>
2025-07-13 13:35:47 +0900

    feat: 로그아웃 기능 구현

--- src/main/java/goormthonuniv/kengdori/backend/controller/KakaoAuthController.java
+++ src/main/java/goormthonuniv/kengdori/backend/controller/KakaoAuthController.java
@@ -6,8 +6,11 @@ import goormthonuniv.kengdori.backend.service.UserServiceImpl;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -43,5 +46,20 @@ public class KakaoAuthController {
             session.setAttribute("tempKakaoId", kakaoId);
             return "redirect:/signup";
         }
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(HttpSession session){
+        Object user = session.getAttribute("login-user");
+
+        if (user != null) {
+            log.info("로그아웃: 세션 사용자 = {}", user);
+            session.invalidate();
+            return ResponseEntity.ok().build();
+        } else {
+            log.info("로그아웃 실패: 세션에 사용자 정보 없음");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
     }
 }
