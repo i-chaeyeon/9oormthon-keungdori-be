a99a5a3e28a770f3ed8e71b81d1dc7ba09b51fe9
i-chaeyeon <leechae0605@naver.com>
2025-11-08 03:09:30 +0900

    fix: 장소 위치 precision 변경

--- src/main/java/goormthonuniv/kengdori/backend/domain/Place.java
+++ src/main/java/goormthonuniv/kengdori/backend/domain/Place.java
@@ -22,7 +22,10 @@ public class Place {
     private String name; // 장소명
     private String address; // 장소 주소
     private String googleId; // 구글 API에서 장소의 id
+
+    @Column(precision = 10, scale = 6)
     private BigDecimal xCoordinate; // x 좌표
+    @Column(precision = 10, scale = 6)
     private BigDecimal yCoordinate; // y 좌표
 
 
