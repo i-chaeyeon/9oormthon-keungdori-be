4c7370699b1640f4cd43d8b4f3793b2829ead271
i-chaeyeon <leechae0605@naver.com>
2025-11-07 22:40:02 +0900

    fix: 위치 기반 조회 시 Getter 누락으로 인한 포맷팅 문제 해결

--- src/main/java/goormthonuniv/kengdori/backend/place/DTO/BoundedPlaceListResponseDTO.java
+++ src/main/java/goormthonuniv/kengdori/backend/place/DTO/BoundedPlaceListResponseDTO.java
@@ -3,7 +3,9 @@ package goormthonuniv.kengdori.backend.place.DTO;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
 @Builder
 @AllArgsConstructor
 public class BoundedPlaceListResponseDTO {
